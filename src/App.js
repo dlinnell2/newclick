5aff11b01a623a98df18d0f87bdac823f5ae7846
Donald Linnell <donald.linnell@gmail.com>
2020-08-19 19:57:53 -0500

    creating info json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Container from './Components/Container'
 import Jumbo from './Components/Jumbo'
+import Card from './Components/Card'
+import characters from './info.json'
 
 class App extends Component{
 
